15875802ffd46854c7dfb0abe989c7693c34b357
iRaySpace <irayspacii@gmail.com>
2022-05-24 16:52:39 +0800

    feat: added camera movement

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -6,14 +6,18 @@ using UnityEngine.UI;
 public class GameSystem : MonoBehaviour
 {
     public Transform playerTransform;
+    public Transform cameraTransform;
     public Text scoreText;
 
     public static int score;
+
+    private float maxPlayerY;
 
     // Start is called before the first frame update
     void Start()
     {
         score = 0; 
+        maxPlayerY = 0;
     }
 
     // Update is called once per frame
@@ -22,5 +26,8 @@ public class GameSystem : MonoBehaviour
         int playerY = Mathf.FloorToInt(playerTransform.position.y);
         score = Mathf.Max(score, playerY);
         scoreText.text = score.ToString();
+
+        maxPlayerY = Mathf.Max(maxPlayerY, playerTransform.position.y);
+        cameraTransform.position = new Vector3(cameraTransform.position.x, maxPlayerY, cameraTransform.position.z);
     }
 }
