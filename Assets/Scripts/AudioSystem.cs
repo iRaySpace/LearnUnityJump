8728d310ea8e77acfac713ca8ff71e3d76744621
irayspace <irayspacii@gmail.com>
2022-05-27 22:35:22 +0800

    feat: added background music

--- Assets/Scripts/AudioSystem.cs
+++ Assets/Scripts/AudioSystem.cs
@@ -11,7 +11,7 @@ public class AudioSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();    
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
